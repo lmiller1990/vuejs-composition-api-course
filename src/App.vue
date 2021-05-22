f04a38c1f52960b6e30e1da7c3eead1babbac769
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-22 17:10:05 +1000

    add timeline

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <section class="section">
+    <div class="container">
+      <timeline />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Timeline from './components/Timeline.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Timeline
   }
 });
 </script>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a>Today</a>
+      <a>This Week</a>
+      <a>This Month</a>
+    </span>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Timeline',
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
