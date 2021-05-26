165f7c4f3de25c64713801f1f41421c6edb7bab6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-26 23:49:18 +1000

    fetch posts with axios

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { reactive, readonly } from 'vue'
+import axios from 'axios'
 import { Post } from './mocks'
 
 interface State {
@@ -24,6 +25,22 @@ class Store {
 
   getState() {
     return readonly(this.state)
+  }
+
+  async fetchPosts() {
+    const response = await axios.get<Post[]>('/posts')
+    const postsState: PostsState = {
+      ids: [],
+      all: new Map,
+      loaded: true
+    }
+
+    for (const post of response.data) {
+      postsState.ids.push(post.id)
+      postsState.all.set(post.id, post)
+    }
+
+    this.state.posts = postsState
   }
 }
 
