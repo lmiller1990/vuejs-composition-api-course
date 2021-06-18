0884b7d3346b8fa6eaaf97b094bdfc8d3f98cb9f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-18 17:07:43 +1000

    update

--- tests/unit/Navbar.spec.ts
+++ tests/unit/Navbar.spec.ts
@@ -3,16 +3,19 @@ import Navbar from '../../src/components/Navbar.vue'
 import { Store } from '../../src/store'
 
 describe('Navbar', () => {
-
-  const store = new Store({
-    posts: {
-      all: new Map(),
-      ids: [],
-      loaded: false
-    }
-  })
-
   it('shows a signup modal via teleport', async () => {
-    const wrapper = mount(Navbar)
+    const store = new Store({
+      posts: {
+        all: new Map(),
+        ids: [],
+        loaded: false
+      }
+    })
+
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [store]
+      }
+    })
   })
 })
