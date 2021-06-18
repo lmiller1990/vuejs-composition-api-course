e605934855f61adc0e9eda533f64dbf42c377fff
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-18 12:27:59 +1000

    add validation

---
+++ src/validation.ts
@@ -0,0 +1,72 @@
+interface Rule {
+  type: 'required' | 'length'
+}
+
+interface Require extends Rule {
+  type: 'required'
+}
+
+interface LengthOptions {
+  min: number
+  max: number
+}
+
+interface Length extends Rule {
+  type: 'length'
+  options: LengthOptions
+}
+
+export function required(): Require {
+  return {
+    type: 'required'
+  }
+}
+
+export function length(options: LengthOptions): Length {
+  return {
+    type: 'length',
+    options
+  }
+}
+
+interface Status {
+  valid: boolean
+  message?: string
+}
+
+type Validator = Require | Length
+
+export function validate(value: string | undefined, validators: Validator[]): Status {
+  for (const validator of validators) {
+    if (validator.type === 'required') {
+      if (!value || value.length === 0) {
+        return {
+          valid: false,
+          message: 'This value is required.'
+        }
+      }
+    }
+
+    if (validator.type === 'length') {
+      if (
+        value!.length < validator.options.min || value!.length > validator.options.max 
+      ) {
+        return {
+          valid: false,
+          message: `The value must be between ${validator.options.min} and ${validator.options.max}`
+        }
+      }
+    }
+  }
+
+  return {
+    valid: true
+  }
+}
+
+// const result =  validate('username', [required()]) {
+//   return {
+//     valid 
+//     message:
+//   }
+// }
