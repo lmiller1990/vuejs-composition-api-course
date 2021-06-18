98f96e9ef8b95c44ac9d83c2ce823efde8e1cc91
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-18 10:40:17 +1000

    form input

---
+++ src/components/FormInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="field">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup (props, ctx) {
+  }
+})
+</script>
