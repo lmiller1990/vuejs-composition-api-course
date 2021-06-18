e2abbca45846c7775c8dbf3f7aa2002eadc95382
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-18 16:36:44 +1000

    update test

--- tests/unit/NewPost.spec.ts
+++ tests/unit/NewPost.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import NewPost from '../../src/components/NewPost.vue'
 
 describe('NewPost', () => {
-  it('creates a post and redirects to /', () => {
+  it('creates a post and redirects to /', async () => {
     mount(NewPost)
   })
 })
