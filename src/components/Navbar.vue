7b4dd07bfc281aca5774667225053359093dc28d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-19 15:31:12 +1000

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="navbar">
     <div class="navbar-end">
-      <div class="buttons">
-        <button
-          class="button"
-          @click="show"
-        >
-          Sign Up
-        </button>
-
+      <div 
+        class="buttons"
+        v-if="auth"
+      >
         <router-link 
           class="button"
           to="/posts/new"
         >
           New Post
         </router-link>
+
+        <button
+          class="button"
+          @click="signOut"
+        >
+          Sign Out
+        </button>
       </div>
+
+      <div 
+        class="buttons"
+        v-else
+      >
+        <button
+          class="button"
+          @click="signUp"
+        >
+          Sign Up
+        </button>
+
+        <button
+          class="button"
+          @click="signIn"
+        >
+          Sign In
+        </button>
+      </div>
+
     </div>
   </div>
 
   <teleport to="#modal">
-    <signup />
+    <component :is="component" />
   </teleport>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, h } from 'vue';
 import Signup from './Signup.vue'
 import { useModal } from '../useModal'
+import { useStore } from '../store'
 
 export default defineComponent({
   components: {
@@ -36,11 +60,36 @@ export default defineComponent({
 
   setup() {
     const modal = useModal()
+    const store = useStore()
+
+    const auth = computed(() => {
+      return !!store.getState().authors.currentUserId
+    })
+
+    const signIn = () => {
+      const Demo = defineComponent({
+        setup () {
+          return () => h('div', 'Demo')
+        }
+      })
+
+      modal.component.value = Demo
+      modal.showModal()
+    }
+
+    const signUp = () => {
+      modal.component.value = Signup
+      modal.showModal()
+    }
+
+    const signOut = () => {}
 
     return {
-      show: () => {
-        modal.showModal()
-      }
+      component: modal.component,
+      signIn,
+      signUp,
+      signOut,
+      auth,
     }
   }
 });
--- src/useModal.ts
+++ src/useModal.ts
@@ -1,9 +1,11 @@
 import { ref } from "vue";
 
 const show = ref(false)
+const component = ref()
 
 export function useModal() {
   return {
+    component,
     show,
     showModal: () => show.value = true,
     hideModal: () => show.value = false,
