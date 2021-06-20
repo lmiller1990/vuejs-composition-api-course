066984aa4c9e5b3a71d520fbc10464928793681c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-20 14:43:54 +1000

    update

--- tests/unit/ShowPost.spec.ts
+++ tests/unit/ShowPost.spec.ts
@@ -1,13 +1,15 @@
 import { mount } from '@vue/test-utils'
 import ShowPost from '../../src/components/ShowPost.vue'
+import { Store } from '../../src/store'
+import { today } from '../../src/mocks'
 
 describe('ShowPost', () => {
-  it('does not show edit button when not authenticated', () => {
+  it('does not show edit button when not authenticated', async () => {
   })
 
-  it('does not show edit button when not authorized', () => {
+  it('does not show edit button when not authorized', async () => {
   })
 
-  it('shows edit button when authorized', () => {
+  it('shows edit button when authorized', async () => {
   })
 })
